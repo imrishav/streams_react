552040acbac48c820b510164baa0cdd3db35ff69
Rishav Sinha <rishav@live.co.uk>
2020-07-10 01:20:53 +0530

    Added Routes and Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const Hats = () => {
-  return <div>Page !</div>;
-};
-
-const Shoes = () => {
-  return <div>Page 222</div>;
-};
+import {
+  StreamCreate,
+  StreamEdit,
+  StreamDelete,
+  StreamShow,
+  StreamList,
+} from './streams';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-          <Route path="/" component={Hats} />
-          <Route path="/e" component={Shoes} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
