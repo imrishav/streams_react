a81653b30dffe6a5cdabdb2b930be82a267dc37f
Rishav Sinha <rishav@live.co.uk>
2020-07-10 23:17:27 +0530

    Added Redux, Added Google O-Auth

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import { SIGN_OUT, SIGN_IN } from './types';
+
+export const signIn = (id) => {
+  return {
+    type: SIGN_IN,
+    payload: id,
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: SIGN_OUT,
+  };
+};
