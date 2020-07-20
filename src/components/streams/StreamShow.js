bfc9f290872d2697397ba3670834f726b61686b0
Rishav Sinha <rishav@live.co.uk>
2020-07-20 21:24:52 +0530

    CRUD Completed

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+const StreamShow = ({ stream, fetchStream, match }) => {
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, []);
+
+  if (!stream) {
+    return (
+      <div>
+        <h1>Loading,,</h1>
+      </div>
+    );
+  }
+
+  const { title, description } = stream;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
