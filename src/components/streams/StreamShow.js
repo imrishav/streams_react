2b7213f78ae416729dd2f3f205d141db04c3abd5
Rishav Sinha <rishav@live.co.uk>
2020-07-20 23:07:08 +0530

    Added Streaming functionality

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
+import flv from 'flv.js';
 
 const StreamShow = ({ stream, fetchStream, match }) => {
+  const videoRef = useRef();
+
+  const mounted = useRef();
+
+  const buildPlayer = () => {
+    if (!stream) {
+      return;
+    }
+    const { id } = match.params;
+    let flvPlayer = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    flvPlayer.attachMediaElement(videoRef.current);
+    flvPlayer.load();
+  };
+
   useEffect(() => {
     fetchStream(match.params.id);
+    // buildPlayer();
+    return () => {};
   }, []);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+    } else {
+      // do componentDidUpate logic
+      buildPlayer();
+    }
+  });
+
+  console.log(mounted);
 
   if (!stream) {
     return (
@@ -18,6 +51,7 @@ const StreamShow = ({ stream, fetchStream, match }) => {
   const { title, description } = stream;
   return (
     <div>
+      <video ref={videoRef} style={{ width: '100%' }} controls />
       <h1>{title}</h1>
       <h5>{description}</h5>
     </div>
